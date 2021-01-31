88042ade886de72a033607996bd0676a3e068ffb
Licsber <licsber@gmail.com>
2021-01-31 09:18:25 +0800

    update by licsber.

---
+++ 839. 相似字符串组/并查集重复优化.py
@@ -0,0 +1,50 @@
+from typing import List
+
+
+class UnionFind:
+    def __init__(self, n: int):
+        self.n = n
+        self.parent = list(range(n))
+        self.size = [1] * n
+        self.setCount = n  # 当前连通分量数目
+
+    def find(self, x: int) -> int:
+        if self.parent[x] == x:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def unite(self, x: int, y: int) -> bool:
+        x, y = self.find(x), self.find(y)
+        if x == y:
+            return False
+
+        if self.size[x] < self.size[y]:
+            x, y = y, x
+        self.parent[y] = x
+        self.size[x] += self.size[y]
+        self.setCount -= 1
+        return True
+
+    def connected(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+
+class Solution:
+    def check(self, a, b) -> bool:
+        cnt = 0
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                cnt += 1
+        return cnt <= 2
+
+    def numSimilarGroups(self, strs: List[str]) -> int:
+        n = len(strs)
+        uf = UnionFind(n)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if uf.connected(i, j):
+                    continue
+                if self.check(strs[i], strs[j]):
+                    uf.unite(i, j)
+        return uf.setCount
