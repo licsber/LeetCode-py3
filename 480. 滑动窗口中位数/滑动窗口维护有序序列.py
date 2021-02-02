c9da900bb6131abd60989fca11cd50d1b2972dd0
Licsber <licsber@gmail.com>
2021-02-03 02:12:51 +0800

    update by licsber.

---
+++ 480. 滑动窗口中位数/滑动窗口维护有序序列.py
@@ -0,0 +1,31 @@
+from bisect import bisect_left
+from typing import List
+
+
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        mid = k // 2
+        if k % 2 == 0:
+            def median(x):
+                return (x[mid - 1] + x[mid]) / 2
+        else:
+            def median(x):
+                return x[mid]
+
+        res = []
+        win = nums[:k]
+        win.sort()
+        res.append(median(win))
+
+        l = len(nums)
+        for i in range(0, l - k):
+            right = nums[i + k]
+            idx = bisect_left(win, right)
+            win.insert(idx, right)
+
+            left = nums[i]
+            idx = bisect_left(win, left)
+            del win[idx]
+
+            res.append(median(win))
+        return res
